4250b876f3a3a864896daa7755eef6f31d07d1d0
kiril <kirill.chugunov0@gmail.com>
2020-11-09 22:20:35 +0400

    added common modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainModule} from './modules/main/main.module';
+import {LoginComponent} from './modules/auth/login/login.component';
+import {RegistrationComponent} from './modules/auth/registration/registration.component';
 
 const routes: Routes = [
   {
     path: '', loadChildren: () => MainModule
-  }
+  },
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
